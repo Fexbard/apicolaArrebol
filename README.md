b0440955444947523937175508f56a258988f70d
fexbard <fexbarreda@gmail.com>
2024-03-07 13:36:38 -0300

    first test

---
+++ README.md
@@ -0,0 +1,16 @@
+Página Apícola Arrebol
+¡Bienvenido a la página apícola Arrebol!
+
+Acerca de Nosotros
+En Arrebol, estamos dedicados a promover la apicultura sostenible y la conservación de las abejas. Nuestra pasión por las abejas y la naturaleza se refleja en cada aspecto de nuestro trabajo, desde la producción de miel hasta la educación y el cuidado de las colmenas.
+
+Nuestros Productos
+Ofrecemos una amplia gama de productos apícolas de alta calidad, incluyendo miel cruda, propóleo, velas de cera de abeja y más. Cada producto se elabora con cuidado y atención para garantizar la máxima frescura y pureza.
+
+Nuestros Servicios
+Además de la venta de productos apícolas, ofrecemos servicios de asesoría apícola y rescate de enjambres. Nuestro equipo de expertos está aquí para ayudarte en todas tus necesidades apícolas, ya sea que estés empezando como apicultor o que necesites ayuda con el manejo de tus colmenas.
+
+Contacto
+¡Estamos aquí para ayudarte! Si tienes alguna pregunta sobre nuestros productos o servicios, no dudes en ponerte en contacto con nosotros a través de nuestro correo electrónico o teléfono. ¡Estamos encantados de hablar contigo!
+
+¡Gracias por visitar la página apícola Arrebol!
---
+++ src/components/Team.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Conócenos</h2>
+          <p>
+          En Arrebol, nos dedicamos a ofrecer productos de alta calidad derivados de la naturaleza. Nos enorgullece trabajar en armonía con las abejas para producir miel y otros productos apícolas de manera sostenible y respetuosa con el medio ambiente. Nuestra pasión por la calidad y la autenticidad impulsa cada paso de nuestro proceso de producción, desde la colmena hasta tu hogar.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img " />
+                    <div className="caption ">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                      <p className="secondJob">{d.secondJob}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+
+        </div>
+      </div>
+    </div>
+  );
+};
